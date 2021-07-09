ae460417d45f714268dd379ee276db81eeb95502
gilescope <gilescope@gmail.com>
2021-07-09 17:41:36 +0100

    Make length configurable

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,11 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 		vscode.workspace.onDidChangeTextDocument(event => {
 		let i = 0;
-		if(event.document != undefined){
-			while(i<event.document.lineCount){
-				if(event.document.lineAt(i).text.length>=255){
+			if (event.document !== undefined) {
+				let config = vscode.workspace.getConfiguration('line-length-highlighter');
+				let max = config.lineLength;
+				while (i < event.document.lineCount) {
+					if (event.document.lineAt(i).text.length >= max) {
 					console.log("A line was changed and caught by line-length-highlighter bitch");
 					let number = (i+1).toString();
 					vscode.window.showInformationMessage("Line " + number + " too long");
